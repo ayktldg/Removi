8d495af49776e5d4927c509c440b21fe75311d79
ayktldg <aykutuludag26@gmail.com>
2022-05-08 00:35:17 +0200

    update foooter with style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,62 +1,74 @@
-import footerSyle from "./Footer.module.css";
+import footerStyle from "./Footer.module.css";
 import layout from "../../style/Layout.module.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin,
+  faCodepen,
+} from "@fortawesome/free-brands-svg-icons";
 const Footer = () => {
   return (
-    <div className={footerSyle.footer}>
+    <footer className={footerStyle.footer}>
       <div className={layout.container}>
-        <div>
-          <small>
-            Powered by
-            <a
-              href="https://www.themoviedb.org/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              TMDB
-            </a>
-          </small>
-        </div>
-        <div className={footerSyle.madeBy}>
-          <small>
-            &copy; 2020 Made by
+        <div className={footerStyle.wrapper}>
+          <div>
+            <small>
+              Powered by
+              <a
+                href="https://www.themoviedb.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span> TMDB</span>
+              </a>
+            </small>
+          </div>
+          <div className={footerStyle.madeBy}>
+            <small>
+              &copy; 2022 Made by
+              <a
+                href="https://github.com/ayktldg"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span> aykutuludag</span>
+              </a>
+            </small>
+          </div>
+          <div className={footerStyle.icons}>
             <a
               href="https://github.com/ayktldg"
               target="_blank"
               rel="noreferrer"
             >
-              ayktldg
+              <FontAwesomeIcon className={footerStyle.icon} icon={faGithub} />
             </a>
-          </small>
-        </div>
-        <div className={footerSyle.icons}>
-          <a href="https://github.com/ayktldg" target="_blank" rel="noreferrer">
-            <i className="fa-brands fa-github"></i>
-          </a>
-          <a
-            href="https://twitter.com/ayktldg26"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fa-brands fa-twitter"></i>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/aykut-uluda%C4%9F-477b17145/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fa-brands fa-twitter"></i>
-          </a>
-          <a
-            href="https://www.codepen.com/ayktldg/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fa-brands fa-codepen"></i>
-          </a>
+            <a
+              href="https://twitter.com/ayktldg26"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon className={footerStyle.icon} icon={faTwitter} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/aykut-uluda%C4%9F-477b17145/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon className={footerStyle.icon} icon={faLinkedin} />
+            </a>
+            <a
+              href="https://www.codepen.com/ayktldg/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon className={footerStyle.icon} icon={faCodepen} />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
